31d7d5a22a9f3513bb38af4e7ea4029f7db29de2
vmandeQB <vmande@quickbase.com>
2021-04-25 01:35:35 -0400

    While and For loops

---
+++ javascript crash course/WhileAndForLoop/home.js
@@ -0,0 +1,19 @@
+// While loops
+
+// var num = 0;
+// while(num < 100){
+
+//     num += 1;
+//     console.log(num);
+// }
+
+
+
+// For loop
+
+// use let instead of var as much as possible.
+
+for(let num =0; num <= 100 ; num ++){
+    console.log(num);
+}
+
